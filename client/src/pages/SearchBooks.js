a5e52562859d0a9cc2ba87a7cc826dee1599d04d
smithnaomi <88051105+smithnaomi@users.noreply.github.com>
2022-01-24 07:18:51 -0500

    unedited

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -60,31 +60,31 @@ const SearchBooks = () => {
     }
   };
 
-  // // create function to handle saving a book to our database
-  // const handleSaveBook = async (bookId) => {
-  //   // find the book in `searchedBooks` state by the matching id
-  //   const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
+  // create function to handle saving a book to our database
+  const handleSaveBook = async (bookId) => {
+    // find the book in `searchedBooks` state by the matching id
+    const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
 
-  //   // get token
-  //   const token = Auth.loggedIn() ? Auth.getToken() : null;
+    // get token
+    const token = Auth.loggedIn() ? Auth.getToken() : null;
 
-  //   if (!token) {
-  //     return false;
-  //   }
+    if (!token) {
+      return false;
+    }
 
-  //   try {
-  //     const response = await saveBook(bookToSave, token);
+    try {
+      const response = await saveBook(bookToSave, token);
 
-  //     if (!response.ok) {
-  //       throw new Error('something went wrong!');
-  //     }
+      if (!response.ok) {
+        throw new Error("something went wrong!");
+      }
 
-  //     // if book successfully saves to user's account, save book id to state
-  //     setSavedBookIds([...savedBookIds, bookToSave.bookId]);
-  //   } catch (err) {
-  //     console.error(err);
-  //   }
-  // };
+      // if book successfully saves to user's account, save book id to state
+      setSavedBookIds([...savedBookIds, bookToSave.bookId]);
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   return (
     <>
