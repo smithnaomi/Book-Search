f4e1493371d528739829edd06392ffbd8eae7ed2
smithnaomi <88051105+smithnaomi@users.noreply.github.com>
2022-01-24 02:02:35 -0500

    added resolvers

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,40 @@
+const { AuthenticationError } = require("apollo-server-express");
+const { User } = require("../models");
+const { signToken } = require("../utils/auth");
+
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id });
+      }
+      throw new AuthenticationError("You must be logged in!");
+    },
+  },
+
+  Mutation: {
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError("No user with that email!");
+      }
+
+      const pw = await user.isCorrectPassword(password);
+
+      if (!pw) {
+        throw new AuthenticationError("Password incorrect!");
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+
+      return { token, user };
+    },
+
+    
