fd1c2bda3f6aa826db485006a46519b405f14d71
smithnaomi <88051105+smithnaomi@users.noreply.github.com>
2022-01-24 00:56:47 -0500

    export remove book

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -43,3 +43,12 @@ export const SAVE_BOOK = gql`
     )
   }
 `;
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: ID!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      savedBooks
+    }
+  }
+`;
